08764d8e940bdb4176ec0c65290832a0da0fc379
Sebastian Milhas <smilhas@itba.edu.ar>
2018-11-03 02:54:40 -0300

    Agrego la parte del padre

--- padre.py
+++ padre.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
+
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.graphics import Line
@@ -12,11 +13,13 @@ from kivy.clock import Clock
 from kivy.graphics import *
 
 from kivy.config import Config
+
 Config.set('graphics', 'position', 'custom')
 Config.set('graphics', 'left', 50)
 Config.set('graphics', 'top',  50)
 Config.set('graphics', 'width', '300')
 Config.set('graphics', 'height', '500')
+
 
 class Painter(Widget):
     
@@ -39,23 +42,52 @@ class LogoScreen(Screen):
         Clock.schedule_once(self.changeScreen, 1)
 
     def changeScreen(self, *args):
-        self.parent.current = "other"
+        self.parent.current = "auth"
 
 class AnotherScreen(Screen):
-    pass
+    def buttonCallback(self):
+        self.parent.current = "option"
+        print("fui a opciones")
+
+class OptionScreen(Screen):
+    def buttonCallback(self):
+        self.parent.current = "main"
+        print("volvi a menu")
+
+class AuthScreen(Screen):
+    def changeScreen(self, *args):
+        self.parent.current = "Home"
+
+class HomeScreen(Screen):
+    def goToCalledScreen(self, nextScreen="History"):
+        self.parent.current = nextScreen
+
+class HistoryScreen(Screen):
+    def goToHomeScreen(self, *args):
+        self.parent.current = "Home"
+
+class ChargeScreen(Screen):
+    def goToHomeScreen(self, *args):
+        self.parent.current = "Home"
+
+class WithdrawScreen(Screen):
+    def goToHomeScreen(self, *args):
+        self.parent.current = "Home"
 
 class ScreenManagement(ScreenManager):
     pass
 
 
-presentation = Builder.load_file('main22.kv')
+
+presentation = Builder.load_file('padre.kv')
+
 
 class MainApp(App):
     def build(self):
         return presentation
-    def printalgo(self):
-        print("algo")
+    
 
 if __name__ == "__main__":
     MainApp().run()
+        
         
